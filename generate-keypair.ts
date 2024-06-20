805a06b7cb2e6d502dd9109b921491a896f4fe9d
silvertoml <silver.toml120@gmail.com>
2024-06-20 02:23:28 -0400

    first

---
+++ generate-keypair.ts
@@ -0,0 +1,13 @@
+import { Keypair } from "@solana/web3.js";
+import "dotenv/config"
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
+
+console.log(keypair);
+
